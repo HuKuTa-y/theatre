1e011da8dbbffca7186569edb1a895a08073009e
HuKuTa-y <zelleonid73@gmail.com>
2025-09-22 12:47:14 +0300

    Update migrations.py

--- server/migrations.py
+++ server/migrations.py
@@ -16,8 +16,8 @@ class MigrationManager:
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS tickets(
                         id SERIAL PRIMARY KEY,
-                        row DECIMAL(10,2) NOT NULL
-                        place DECIMAL(10,2) NOT NULL
+                        row DECIMAL(10,2) NOT NULL,
+                        place DECIMAL(10,2) NOT NULL,
                         name_movie VARCHAR(100) NOT NULL,
                         price DECIMAL(10, 2) NOT NULL
                         )
